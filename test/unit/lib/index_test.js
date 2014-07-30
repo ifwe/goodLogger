dce5a1910375631a55ed67a1439392d98d79868a
npolkowske <npolkowske@tagged.com>
2014-07-30 16:00:43 -0700

    started unittests

--- test/unit/lib/index_test.js
+++ test/unit/lib/index_test.js
@@ -1,15 +1,14 @@
-/*jshint expr: true*/
+var goodLogger = require(LIB_DIR);
 
 describe('goodLogger', function() {
-    beforeEach(function() {
-
+    it('returns goodLogger', function(){
+        goodLogger.should.be.a('object');
     });
 
-    afterEach(function() {
-
+    it('creates file without a path argument', function() {
+        //var log = log();
     });
 
-    it('should do something', function() {
-        console.log(process.env.OLDPWD);
+    it('creates file with a path argument', function() {
     });
 });
