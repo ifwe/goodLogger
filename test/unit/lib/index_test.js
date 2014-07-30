52572f42ef8a4d74ea7a641bf1897120f0917239
npolkowske <npolkowske@tagged.com>
2014-07-30 16:45:17 -0700

    exported module and started adding tests

--- test/unit/lib/index_test.js
+++ test/unit/lib/index_test.js
@@ -1,14 +1,27 @@
 var goodLogger = require(LIB_DIR);
+var touch = require('touch');
+var fs = require('fs');
 
 describe('goodLogger', function() {
+    beforeEach(function(){
+        this.logger = goodLogger();
+        this.fs = sinnon.spy();
+    });
+
+    afterEach(function(){
+        this.logger = {};
+    });
+
     it('returns goodLogger', function(){
-        goodLogger.should.be.a('object');
+        this.logger.should.be.a('object');
+        this.logger.should.have.property('info');
+        this.logger.should.have.property('error');
     });
 
     it('creates file without a path argument', function() {
-        //var log = log();
+
     });
 
-    it('creates file with a path argument', function() {
-    });
+    it('creates file with a path argument', function() {});
+
 });
