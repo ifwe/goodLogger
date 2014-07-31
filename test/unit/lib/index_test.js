f8b7e3cd9f2b9343213840a4a5cf82579e498d66
npolkowske <npolkowske@tagged.com>
2014-07-30 18:18:39 -0700

    added some more tests

--- test/unit/lib/index_test.js
+++ test/unit/lib/index_test.js
@@ -1,27 +1,46 @@
-var goodLogger = require(LIB_DIR);
-var touch = require('touch');
 var fs = require('fs');
+var goodLogger = require(LIB_DIR);
+var path = require('path');
+var touch = require('touch');
 
 describe('goodLogger', function() {
     beforeEach(function(){
         this.logger = goodLogger();
-        this.fs = sinnon.spy();
+
+        fs.existsSync = sinon.spy();
+        fs.mkdirSync = sinon.spy();
+        touch.sync = sinon.spy();
+        path.dirname = sinon.spy();
     });
 
-    afterEach(function(){
+    afterEach(function() {
         this.logger = {};
+        fs.rmdir(LIB_DIR + "/log/error_log");
     });
 
-    it('returns goodLogger', function(){
+    it('returns winston instance', function() {
         this.logger.should.be.a('object');
         this.logger.should.have.property('info');
         this.logger.should.have.property('error');
     });
 
-    it('creates file without a path argument', function() {
-
+    it('checks to see if directory exists', function() {
+        path.dirname.called.should.be.true;
+        fs.existsSync.called.should.be.true;
+        touch.sync.called.should.be.true;
     });
 
-    it('creates file with a path argument', function() {});
+    it('creates a directory and file without a path argument', function() {
+        this.logger.transports.file.filename.should.equal('error_log');
+        this.logger.transports.file.dirname.should.contain('/log');
+    });
+
+    it('creates file with a path argument', function() {
+        var mockPath = new goodLogger('/foo/bar/baz_log');
+
+        mockPath.transports.file.filename.should.equal('baz_log');
+        mockPath.transports.file.dirname.should.contain('bar');
+
+    });
 
 });
